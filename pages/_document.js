40ae08a9e3f8d57679a1d215d982e55b9eeaee01
infoagrotechinsights-cmd <infoagrotechinsights@gmail.com>
2026-05-12 03:57:13 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="tr">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <meta name="description" content="ClinicAI V3 - AI Destekli Klinik Yönetim Platformu" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
 
